9e245f8b76088258c75aa73939d1b1f16f527b21
Martin Butt <martin@kiva.org>
2013-10-02 22:43:36 -0700

    Custom PHPCS sniffs

---
+++ Sniffs/Functions/DisallowedFunctionsSniff.php
@@ -0,0 +1,24 @@
+<?php
+
+class Kiva_Sniffs_Functions_DisallowedFunctionsSniff implements PHP_CodeSniffer_Sniff {
+
+	public $supportedTokenizers = array('PHP');
+
+	public function register() {
+	    return array(T_STRING);
+	}
+
+	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
+		static $reported = array();
+
+		$all_tokens = $phpcsFile->getTokens();
+
+		if ($all_tokens[$stackPtr]['content'] == 'elpr') {
+			$lno = $all_tokens[$stackPtr]['line'];
+			if (!isset($reported[$lno])) {
+				$reported[$lno] = true;
+				$phpcsFile->addError('elpr() function left in code', $stackPtr);
+			}
+		}
+	}
+}
