928fde3fb151ae5c40a3410498f0ad430479b532
Martin Butt <martin@kiva.org>
2015-03-02 15:03:54 -0800

    Added $_SERVER['REMOTE_ADDR'] check

--- phpcs/Kiva/Sniffs/Functions/DisallowedVariablesSniff.php
+++ phpcs/Kiva/Sniffs/Functions/DisallowedVariablesSniff.php
@@ -5,7 +5,7 @@ class Kiva_Sniffs_Functions_DisallowedVariablesSniff implements PHP_CodeSniffer_
 	public $supportedTokenizers = array('PHP');
 
 	public function register() {
-	    return array(T_VARIABLE);
+	    return array(T_VARIABLE, T_CONSTANT_ENCAPSED_STRING);
 	}
 
 	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
@@ -18,6 +18,16 @@ class Kiva_Sniffs_Functions_DisallowedVariablesSniff implements PHP_CodeSniffer_
 			if (!isset($reported[$lno])) {
 				$reported[$lno] = true;
 				$phpcsFile->addError('Accessing $_COOKIE directly is forbidden. Use Bc_Cookie instead.', $stackPtr);
+			}
+		} else if ($all_tokens[$stackPtr]['content'] == '$_SERVER') {
+			if ($all_tokens[$stackPtr+2]['type'] == 'T_CONSTANT_ENCAPSED_STRING'
+				&& strstr($all_tokens[$stackPtr+2]['content'], 'REMOTE_ADDR')) {
+
+				$lno = $all_tokens[$stackPtr]['line'];
+				if (!isset($reported[$lno])) {
+					$reported[$lno] = true;
+					$phpcsFile->addError('Accessing $_SERVER[\'REMOTE_ADDR\'] directly is forbidden. Use Bc_DeviceDetect instead.', $stackPtr);
+				}
 			}
 		}
 	}
