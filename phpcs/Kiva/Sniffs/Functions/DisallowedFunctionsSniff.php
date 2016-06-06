ed177f29789d97f7f1dea9f5470c7f834dc9aea7
Tim Ledlie <ledlie@kiva.org>
2016-06-06 12:02:03 -0400

    Add strtotime() to forbidden functions

--- phpcs/Kiva/Sniffs/Functions/DisallowedFunctionsSniff.php
+++ phpcs/Kiva/Sniffs/Functions/DisallowedFunctionsSniff.php
@@ -7,6 +7,7 @@ class Kiva_Sniffs_Functions_DisallowedFunctionsSniff implements PHP_CodeSniffer_
 	private $forbiddenTokens = array(
 		 'elpr'                  => 'elpr() function left in code'
 		,'time'                  => 'Usage of time() forbidden. Use Bc_Date::now()->asTimestamp() instead.'
+		,'strtotime'             => 'Usage of strtotime() forbidden. Use Bc_Date::createFromString()->asTimestamp() instead.'
 		,'error_log'             => 'Usage of error_log() forbidden. Use Bc_Logger instead with the right channel.'
 		,'extract'               => 'Usage of extract() forbidden.'
 		,'session_id'            => 'Usage of session_id() forbidden. Use Bc_Session::getId() instead.'
